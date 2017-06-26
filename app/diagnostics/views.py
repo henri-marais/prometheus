b5837e29fb445b7e900cdc1ac79f0dd1d1cccfe7
Henri <henri.marais@gmail.com>
2017-06-26 21:52:56 +0200

    fixed string formatting error in the task feedback loop

--- app/diagnostics/views.py
+++ app/diagnostics/views.py
@@ -96,7 +96,7 @@ def machine_scanner(task_id):
         'total_run_time': task.info.get('total_run_time'),
         'current_run_time': task.info.get('current_run_time'),
         'cycles': task.info.get('cycles'),
-        'motor_current': "{:2.2f} A".format(task.info.get('motor_current')),
+        'motor_current': "%s A" % task.info.get('motor_current'),
         'average_current':'',
         'state':task.info.get('state'),
         'worker_state': task.state}
